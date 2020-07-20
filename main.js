d709fb9744b3294d0f68f0c78a8c9bb6afff7bd1
Isaac Rodriguez <isaac_rodriguez@live.com>
2020-07-20 12:44:04 -0400

    Changing startup to navigate to web

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://www.bing.com')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
